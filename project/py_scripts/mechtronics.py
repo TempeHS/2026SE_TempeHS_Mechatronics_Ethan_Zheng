566dea8949ea35e9746da802d2f1e4809369b990
The-fool-with-great-luck <ethan.zheng1@education.nsw.gov.au>
2025-08-15 13:21:42 +1000

    finished movement

--- project/py_scripts/mechtronics.py
+++ project/py_scripts/mechtronics.py
@@ -1,49 +1,77 @@
 import time
 from machine import Pin, PWM
 from servo import Servo
-from machine import RTC
 
-"""
-Make a class
-"""
+class MechMovement:
+    def __init__(self):
+        
+        servo_pwmL = PWM(Pin(16))
+        servo_pwmR = PWM(Pin(15)) 
 
-servo_pwmL = PWM(Pin(16))
-servo_pwmR = PWM(Pin(15))
+        freq = 50          
+        min_us = 500         
+        max_us = 2500        
+        dead_zone_us = 1500     
+        
+        self.left_motor = Servo(
+            pwm=servo_pwmL,
+            min_us=min_us,
+            max_us=max_us, 
+            dead_zone_us=dead_zone_us,
+            freq=freq
+        )
+        
+        self.right_motor = Servo(
+            pwm=servo_pwmR,
+            min_us=min_us,
+            max_us=max_us,
+            dead_zone_us=dead_zone_us, 
+            freq=freq
+        )
+        
+        # Starts
+        self.stop()
+        print("Ready!")
+    
+    def stop(self):
+        self.left_motor.set_duty(1500)
+        self.right_motor.set_duty(1500)
+        print("Stops movement")
+    
+    def move_backward(self):
+        self.left_motor.set_duty(500)  
+        self.right_motor.set_duty(2500)  
+        print("Moving forward")
+    
+    def move_forward(self): 
+        self.left_motor.set_duty(2500)
+        self.right_motor.set_duty(500)
+        print("Reverse")
+    
 
-freq = 50
-min_us = 500
-max_us = 2500
-dead_zone_us = 1500
+    def turn_right(self):
+        self.left_motor.set_duty(1800)   
+        self.right_motor.set_duty(1800)  
+        print("Turning right")
+    
+    def turn_left(self):
+        self.left_motor.set_duty(1200)  
+        self.right_motor.set_duty(1200) 
+        print("Turning left")
 
-my_servo = Servo(
-    pwm=servo_pwmL, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
-    )
-my_servo2 = Servo(
-    pwm=servo_pwmR, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
-    )
+if __name__ == "__main__":
+    robot = MechMovement()
+    
+    #robot.move_backward()
+    #time.sleep(1)
+    
+    robot.turn_right()
+    time.sleep(0.9)
+    
+    #robot.move_forward() 
+    #time.sleep(1)
 
-def stop():
-    #stop
-    stop()
-
-def Forward():
-    my_servo.set_duty(500)
-    my_servo2.set_duty(2500)
-    time.sleep(2)
-
-def Backward():
-    my_servo.set_duty(2500)
-    my_servo2.set_duty(500)
-    time.sleep(2)
-
-def Left():
-    my_servo.set_duty(2500)
-    my_servo2.set_duty(2500)
-    time.sleep(2)
-
-
-def Right():
-    my_servo.set_duty(500)
-    my_servo2.set_duty(500)
-    time.sleep(2)
-
+    #robot.turn_left()
+    #time.sleep(0.9)
+    
+    robot.stop()
